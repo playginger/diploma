59eaf50f19052a4f35b07b74b18e017874b0bd10
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-10-23 23:32:51 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -7,7 +7,6 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=15, unique=True)
 
 
-
 # Модель записи
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
