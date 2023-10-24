6153d05f5f4014fba7dff55105ba9f5f32bfa101
Серж Рыж (playginger) <7playginger7@gmail.com>
2023-10-24 20:37:52 +0300

    начало диплома

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,12 @@
 from .apps import UsersConfig
 
-app_name = UsersConfig.name
+from django.urls import path
+from .views import registration_view, login_view, create_post_view, view_post_view
 
+app_name = UsersConfig.name
 urlpatterns = [
+    path('register/', registration_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('create/', create_post_view, name='create_post'),
+    path('post/<int:post_id>/', view_post_view, name='view_post'),
 ]
